b7fca85e7f6d903ed233131e98169a9abd721519
Simon Weiß <simon.weiss@alexanderthamm.com>
2022-12-29 14:32:44 +0100

    Initial commit

---
+++ src/main.rs
@@ -0,0 +1,32 @@
+use std::{
+    thread::sleep,
+    time::{Duration, Instant},
+};
+
+#[tokio::main]
+async fn main() {
+    println!("Start ...");
+    let start = Instant::now();
+
+    // Spawns task in background thread
+    let io_task = tokio::spawn(async_io_func());
+
+    let syn_res = sync_func();
+    println!("{:?}: {}", start.elapsed(), syn_res);
+
+    // will print shortly after sync because much works has already be done in the background
+    let io_res = io_task.await.unwrap();
+    println!("{:?}: {}", start.elapsed(), io_res);
+
+    println!("{:?}: I'm last 🦀", start.elapsed());
+}
+
+fn sync_func() -> String {
+    sleep(Duration::from_secs(2));
+    String::from("Sync Result")
+}
+
+async fn async_io_func() -> String {
+    sleep(Duration::from_secs(3));
+    String::from("Async Result")
+}
